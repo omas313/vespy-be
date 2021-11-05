132378254fa184df11fd2fe038e9038a3c59f133
Omar <opace88@gmail.com>
2021-11-05 23:29:20 +0400

    Create db seed

--- seed.js
+++ seed.js
@@ -1,56 +1,66 @@
-const { Genre } = require("./models/genre");
-const { Movie } = require("./models/movie");
+const { Model } = require("./models/model");
+const { Vespa } = require("./models/vespa");
 const mongoose = require("mongoose");
 const config = require("config");
 
 const data = [
   {
-    name: "Comedy",
-    movies: [
-      { title: "Airplane", numberInStock: 5, dailyRentalRate: 2 },
-      { title: "The Hangover", numberInStock: 10, dailyRentalRate: 2 },
-      { title: "Wedding Crashers", numberInStock: 15, dailyRentalRate: 2 }
-    ]
+    nome: "Piaggio Vespa Granturismo 200",
+    cilindrata: 200,
+    vespe: [
+      { km: 100, tariffe: 150 },
+      { km: 3454, tariffe: 120 },
+      { km: 100345, tariffe: 60 },
+      { km: 92234, tariffe: 60 },
+    ],
   },
   {
-    name: "Action",
-    movies: [
-      { title: "Die Hard", numberInStock: 5, dailyRentalRate: 2 },
-      { title: "Terminator", numberInStock: 10, dailyRentalRate: 2 },
-      { title: "The Avengers", numberInStock: 15, dailyRentalRate: 2 }
-    ]
+    nome: "Piaggio Vespa PX 125",
+    cilindrata: 125,
+    vespe: [
+      { km: 23000, tariffe: 45 },
+      { km: 300, tariffe: 65 },
+      { km: 0, tariffe: 80 },
+    ],
   },
   {
-    name: "Romance",
-    movies: [
-      { title: "The Notebook", numberInStock: 5, dailyRentalRate: 2 },
-      { title: "When Harry Met Sally", numberInStock: 10, dailyRentalRate: 2 },
-      { title: "Pretty Woman", numberInStock: 15, dailyRentalRate: 2 }
-    ]
+    nome: "Noleggio Vespa 50",
+    cilindrata: 50,
+    vespe: [
+      { km: 0, tariffe: 50 },
+      { km: 12340, tariffe: 30 },
+    ],
   },
   {
-    name: "Thriller",
-    movies: [
-      { title: "The Sixth Sense", numberInStock: 5, dailyRentalRate: 2 },
-      { title: "Gone Girl", numberInStock: 10, dailyRentalRate: 2 },
-      { title: "The Others", numberInStock: 15, dailyRentalRate: 2 }
-    ]
-  }
+    nome: "Vespa Sprint 125 3V Sport",
+    cilindrata: 124,
+    vespe: [
+      { km: 456, tariffe: 100 },
+      { km: 9876, tariffe: 80 },
+    ],
+  },
 ];
 
 async function seed() {
   await mongoose.connect(config.get("db"));
 
-  await Movie.deleteMany({});
-  await Genre.deleteMany({});
+  await Vespa.deleteMany({});
+  await Model.deleteMany({});
 
-  for (let genre of data) {
-    const { _id: genreId } = await new Genre({ name: genre.name }).save();
-    const movies = genre.movies.map(movie => ({
-      ...movie,
-      genre: { _id: genreId, name: genre.name }
+  for (let model of data) {
+    const { modelloId } = await new Model({
+      nome: model.nome,
+      cilindrata: model.cilindrata,
+    }).save();
+    const vespe = model.vespe.map(vespa => ({
+      ...vespa,
+      modello: {
+        _id: modelloId,
+        nome: model.nome,
+        cilindrata: model.cilindrata,
+      },
     }));
-    await Movie.insertMany(movies);
+    await Vespa.insertMany(vespe);
   }
 
   mongoose.disconnect();
